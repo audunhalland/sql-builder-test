47f08b1822c79316e4dc8577d36df8fc3bc1c84e
Audun Halland <audun.halland@gmail.com>
2020-05-03 21:31:26 +0200

    Restructure

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-use proc_macro::TokenStream;
-use quote::quote;
+#![allow(dead_code)]
 
-#[allow(dead_code)]
+use proc_macro::TokenStream;
+
+mod blocks;
 mod builder;
 mod codegen;
 mod parse;
 
 #[proc_macro]
-pub fn build_query(_input: TokenStream) -> TokenStream {
-    let dummy = quote! { "lol" };
-    dummy.into()
+pub fn build_query(input: TokenStream) -> TokenStream {
+    let ast: parse::BuilderAST = syn::parse_macro_input!(input);
+    codegen::codegen(ast).into()
 }
